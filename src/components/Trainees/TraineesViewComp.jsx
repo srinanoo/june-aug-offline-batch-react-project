6f5aa9f591f2d4b9e379a738286d5a9c3563f655
srinanoo <srinanoo@gmail.com>
2024-03-29 23:55:08 +0530

    29-03-2024

--- src/components/Trainees/TraineesViewComp.jsx
+++ src/components/Trainees/TraineesViewComp.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom";
 
-const API_URL = await import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default function TraineesViewComp() {
 
